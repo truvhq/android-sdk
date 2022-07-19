3d91404bd70cff24248cac7bc37178765ad9a9c7
insidefun <eugene.dedkovsky@gmail.com>
2022-07-20 00:33:41 +0200

    Added SuccessPayload param into onSuccess event

--- truv_bridge/src/main/kotlin/com/truv/models/TruvSuccessPayload.kt
+++ truv_bridge/src/main/kotlin/com/truv/models/TruvSuccessPayload.kt
@@ -1,5 +1,9 @@
 package com.truv.models
 
+import org.json.JSONException
+import org.json.JSONObject
+import kotlin.jvm.Throws
+
 data class TruvSuccessPayload(
     val publicToken: String,
     val metadata: Metadata
@@ -9,5 +13,25 @@ data class TruvSuccessPayload(
         val taskId: String,
         val employer: TruvEmployer?
     )
+
+    companion object {
+
+        @Throws(JSONException::class)
+        fun fromJson(json: String): TruvSuccessPayload {
+            val payloadJson = JSONObject(json)
+
+            val publicToken = payloadJson.getString("public_token")
+
+            val metadataJson = payloadJson.getJSONObject("metadata")
+            val taskId = metadataJson.getString("task_id")
+            val employerJson = metadataJson.getJSONObject("employer")
+            val employerName = employerJson.getString("name")
+
+            val metadata = Metadata(taskId, TruvEmployer(employerName))
+
+            return TruvSuccessPayload(publicToken, metadata)
+        }
+
+    }
 
 }
