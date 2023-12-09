38e1c1fe651fb85b2af555ef4911030bb94eabe7
insidefun <truv@gmail.com>
2023-12-09 16:19:25 +0800

    Moved TruWebViewClient and WebAppInterface into separate classes; Implement support of External login event

--- truv_bridge/src/main/kotlin/com/truv/webview/TruvWebViewClient.kt
+++ truv_bridge/src/main/kotlin/com/truv/webview/TruvWebViewClient.kt
@@ -1,65 +1,37 @@
 package com.truv.webview
 
 import android.content.Context
-import android.util.Log
+import android.content.Intent
 import android.webkit.WebResourceError
 import android.webkit.WebResourceRequest
-import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import android.webkit.WebViewClient
 
-
 class TruvWebViewClient(
     private val context: Context,
-    private val eventListeners: Set<TruvEventsListener>,
-    private val onLoading: (Boolean) -> Unit = {},
-    private val onLoaded: () -> Unit = {},
-    private val openExternalLinkInAppBrowser: ((String) -> Unit)? = null,
+    private val eventListeners: Set<TruvEventsListener>
 ) : WebViewClient() {
-    private val seenURLs = mutableSetOf<String>()
-
-    override fun shouldInterceptRequest(
-        view: WebView?,
-        request: WebResourceRequest
-    ): WebResourceResponse? {
-        if (request.requestHeaders.containsKey("X-Requested-With")) {
-            request.requestHeaders.remove("X-Requested-With")
-        }
-        return super.shouldInterceptRequest(view, request)
-    }
 
     override fun shouldOverrideUrlLoading(
         view: WebView?,
         request: WebResourceRequest?
     ): Boolean {
-        Log.d("TruvWebViewClient", "URL: ${request?.url}")
-        onLoading(true)
         request?.let {
-            openExternalLinkInAppBrowser?.invoke(it.url.toString()) ?: run {
-                view?.loadUrl(it.url.toString())
-            }
+            view?.loadUrl(it.url.toString())
         }
-
-        seenURLs.add(request?.url.toString())
+//        val i = Intent(Intent.ACTION_VIEW)
+//        i.data = request?.url
+//
+//        context.startActivity(i)
         return true
     }
 
-    override fun onPageCommitVisible(view: WebView?, url: String?) {
-        super.onPageCommitVisible(view, url)
-        onLoading(false)
-        onLoaded()
-    }
-
-    override fun doUpdateVisitedHistory(view: WebView?, url: String?, isReload: Boolean) {
-        Log.d("TruvWebViewClient", "doUpdateVisitedHistory: $url")
-        if (url != null) {
-            seenURLs.add(url)
-        }
-        super.doUpdateVisitedHistory(view, url, isReload)
-    }
-
-    fun getSeenPages(): Set<String> {
-        return seenURLs
+    override fun onReceivedError(
+        view: WebView?,
+        request: WebResourceRequest?,
+        error: WebResourceError?
+    ) {
+        eventListeners.forEach { it.onClose() }
     }
 
 }
